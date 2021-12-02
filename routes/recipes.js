f8a0d85fe7efeaf3e3acc6b5c19d184213b93b69
Anna Velarde <annavelardevilaseca@gmail.com>
2021-12-02 21:17:05 +0000

    id working homepage

--- routes/recipes.js
+++ routes/recipes.js
@@ -25,11 +25,52 @@ router.post("/create", isLoggedIn, (req, res) => {
   })
     .then((createRecipe) => {
       console.log(createRecipe);
-      // res.json({ recipes: createRecipe });
+      res.json({ recipes: createRecipe });
     })
     .catch((e) => {
       console.log(e);
       res.status(500).json({ errorMessage: "Something fed up" });
+    });
+});
+
+// router.get("/:recipeId", (req, res) => {
+//   const { recipeId } = req.params;
+
+//   const singleRecipe = allRecipes.find((element) => element.id === recipeId);
+//   if (!singleRecipe) {
+//     return res
+//       .status(404)
+//       .json({ errorMessage: `Recipe with the id ${recipeId} does not exist` });
+//   }
+//   res.json({ recipe: singleRecipe });
+// });
+
+// router.get("/:recipeId", (req, res) => {
+//   const { recipeId } = req.params;
+
+//   Recipe.findById(recipeId)
+//   .populate("owner")
+//   .then((singleRecipe) => {
+//   if (!singleRecipe) {
+//     return res
+//       .status(404)
+//       .json({ errorMessage: `Recipe with the id ${recipeId} does not exist` });
+//   }
+//   res.json({ recipe: singleRecipe });
+// });
+
+router.get("/:dynamic", (req, res) => {
+  const { dynamic } = req.params;
+
+  Recipe.findById(dynamic)
+    .populate("owner")
+    .then((singleRecipe) => {
+      if (!singleRecipe) {
+        return res.status(404).json({
+          errorMessage: `Recipe with the id ${dynamic} does not exist`,
+        });
+      }
+      res.json({ recipe: singleRecipe });
     });
 });
 

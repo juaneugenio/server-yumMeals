3618fdbfc08e22f742d7343ac47adb29e1c79e68
juaneugenio <leuviah77@gmail.com>
2021-12-02 14:50:32 +0100

    Added email to UserModel

--- models/User.model.js
+++ models/User.model.js
@@ -7,6 +7,11 @@ const userSchema = new Schema(
       type: String,
       // unique: true -> Ideally, should be unique, but its up to you
     },
+    email: {
+      type: String,
+      // required: true,
+      // unique: true,
+    },
     password: String,
   },
   {

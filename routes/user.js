5d46cb75660eef7319d2ab41da796dbd8ea1bad3
juaneugenio <leuviah77@gmail.com>
2021-12-14 22:37:12 +0100

    Continue working on the delete function in userjs

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const upload = require("../middleware/cloudinary");
 const router = require("express").Router();
 const User = require("../models/User.model.js");
+const Session = require("../models/Session.model");
+const Recipe = require("../models/Recipe.model");
+// const Rating = require("../models/Rating.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 //method .single. it expect a fiel name which the value is a string. it`s the fiel name of our form that it will hold our image
@@ -28,12 +31,33 @@ router.patch(
 
 //Deleting user
 
-router.delete("/:userId", isLoggedIn, (req, res) => {
-  const { userId } = req.params;
+// router.delete("/:userId", isLoggedIn, (req, res) => {
+//   const { userId } = req.params;
 
-  User.findByIdAndDelete(userId).then((deletedUser) => {
-    res.json({ succes: true });
-  });
-});
+//   User.findByIdAndDelete(userId).then((deletedUser) => {
+//     res.json({ succes: true });
+//   });
+// });
+
+router.delete("/:userId"),
+  isLoggedIn,
+  async (req, res) => {
+    console.log("------>", req.user);
+    const { userId } = req.params;
+    const arrayUserRecipes = await Recipe.find({ owner: userId });
+    const allUserRecipes = arrayUserRecipes.map((theRecipe) => theRecipe._id);
+
+    const userSession = await Session.findById(req.headers.authorization).populate(
+      "user"
+    );
+
+    if (req.user._id === req.session) {
+      await Promise.all([
+        User.findByIdAndDelete(userId),
+        Recipe.deleteMany({ _id: { $in: allUserRecipes } }),
+        Session.findByIdAndDelete(req.headers.authorization),
+      ]);
+    }
+  };
 
 module.exports = router;

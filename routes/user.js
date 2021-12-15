c9b2afddedc621bfdf9c2658ae692a03d36af656
juaneugenio <leuviah77@gmail.com>
2021-12-15 18:04:13 +0100

    user profile update done without styling

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,27 @@ const Recipe = require("../models/Recipe.model");
 // const Rating = require("../models/Rating.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
+router.patch("/edit-profile", isLoggedIn, (req, res) => {
+  const { username } = req.body;
+  const { _id } = req.user;
+
+  if (username === req.user.username) {
+    return res.json({ user: req.user });
+  }
+
+  User.findOne({ username }).then((foundedUser) => {
+    if (foundedUser) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Username already exit. Insert another one" });
+    }
+
+    User.findByIdAndUpdate(_id, { username }, { new: true }).then((updatedUser) => {
+      res.json({ user: updatedUser });
+    });
+  });
+});
+
 //method .single. it expect a fiel name which the value is a string. it`s the fiel name of our form that it will hold our image
 router.patch(
   "/updateProfileImage",
@@ -42,22 +63,33 @@ router.patch(
 router.delete("/:userId"),
   isLoggedIn,
   async (req, res) => {
-    console.log("------>", req.user);
     const { userId } = req.params;
     const arrayUserRecipes = await Recipe.find({ owner: userId });
     const allUserRecipes = arrayUserRecipes.map((theRecipe) => theRecipe._id);
+    console.log({ theRecipe });
 
-    const userSession = await Session.findById(req.headers.authorization).populate(
-      "user"
-    );
+    // const userSession = await Session.findById(req.headers.authorization).populate(
+    //   "user"
+    // )
 
-    if (req.user._id === req.session) {
-      await Promise.all([
-        User.findByIdAndDelete(userId),
-        Recipe.deleteMany({ _id: { $in: allUserRecipes } }),
-        Session.findByIdAndDelete(req.headers.authorization),
-      ]);
-    }
+    const userSession = req.headers.authorization;
+
+    Session.findById(userSession)
+      .populate("user")
+      .then((session) => {
+        if (session.user !== user._id) {
+          return res.status(404).json({
+            errorMessage: "You are not aloud to do this",
+          });
+        }
+        return res.status(200).json(session);
+      });
+
+    // await Promise.all([
+    //   Recipe.deleteMany({ _id: { $in: allUserRecipes } }),
+    //   User.findByIdAndDelete(userId),
+    //   Session.findByIdAndDelete(req.headers.authorization),
+    // ]);
   };
 
 module.exports = router;
